38fb07e8cdab2d883b27b81a8605fbbe52518c9e
munvoseli <muslinmelody@gmail.com>
2021-01-29 19:08:23 -0500

    Reworked text formatter

--- txtformat.js
+++ txtformat.js
@@ -1,55 +1,54 @@
-// It should be noted that this program freezes my Ubuntu 20.04 when using Firefox
 var fileInput = document.createElement("input");
 fileInput.setAttribute("type", "file");
 fileInput.multiple = true;
 function loadFile(file) {
     var fileReader = new FileReader();
     fileReader.addEventListener("loadend", function(e) {
+	console.log("hi");
 	var result = fileReader.result;
 	result = result.replaceAll("\u2014", "--");
 	result = result.replaceAll("\u2019", "'");
 	result = result.replaceAll("\u201c", '"');
 	result = result.replaceAll("\u201d", '"');
 	result = result.replaceAll("\u2026", '...');
-	var paras = result.split(/\n[\n ]+/);
+	//var paras = result.split(/\n/);
 	var lineLength = 30;
-	for (var k = 0; k < paras.length; k++)
+	var lines = [];
+	var lastBreak = 0;
+	var breaks = [0];
+	while (lastBreak + lineLength < result.length)
 	{
-	    var words = paras[k].split(" ");
-	    var lines = [""];
-	    var line = 0;
-	    for (var i = 0; i < words.length; i++)
+	    var j = result.indexOf("\n", lastBreak + 1);
+	    if (j < lastBreak + lineLength) // there's another break in time
 	    {
-		var word = words[i];
-		if (lines[line].length + word.length < lineLength)
-		{
-		    lines[line] += (lines[line].length ? " " : "") + word;
-		}
-		else
-		{
-		    line++;
-		    i--;
-		    lines[line] = [];
-		}
+		lastBreak = j;
 	    }
-	    paras[k] = lines.join("\n");
+	    else
+	    {
+		var i = result.lastIndexOf(" ", lastBreak + lineLength);
+		if (i <= lastBreak)
+		    i = lastBreak + lineLength;
+		breaks.push(i);
+		lastBreak = i;
+	    }
 	}
-	result = paras.join("\n");
-	var lines = result.split("\n");
-	for (var line = 0; line < lines.length; line++)
-	    for (var j = lines[line].length; j < lineLength; j++)
-		lines[line] += " ";
-	console.log(lines.length);
+	breaks.push(result.length);
+	var substrings = [];
+	for (var i = 0; i < breaks.length - 1; i++)
+	    substrings.push(result.substring(breaks[i], breaks[i + 1]));
+	console.log(substrings);
+	lines = substrings.join("\n").split("\n");
+	console.log(lines);
+	//var lines = paras.join("\n").split("\n");
+	for (var i = 0; i < lines.length; i++)
+	    while (lines[i].length < lineLength)
+		lines[i] += " ";
 	result = lines.join("\n");
 	for (var i = 0; i < result.length; i++)
-	{
 	    if (result.charCodeAt(i) >= 128)
 		console.log(result[i]);
-	}
 	console.log(result);
     }, false);
-    
-    // only choose one of these
     fileReader.readAsText(file);
 }
 fileInput.addEventListener("change", function() {
