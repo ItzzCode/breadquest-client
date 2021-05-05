c36f768221d021b8ff86c65f62af68c0af5ca19c
xuncoseli <muslinmelody@gmail.com>
2021-05-05 15:49:53 -0400

    client script

--- myjs.js
+++ myjs.js
@@ -1,3 +1,4 @@
+// https://discord.gg/q4NBCjsg
 // asdf to walk
 // jkl; to mine/place
 // m to lock walk
@@ -20,12 +21,41 @@ Mel2 | -1731, -40
 Mel3 | -2024, -410
 Mel4 / rainbow lounge | -23, 5908
 Mel5 | -59, 7651
+Mel6 | 47, 22628
 Wikitown | 15701, -596
 Small Wikitown | 9103, -617
-Boteram cave | 16044, 797`;
-
+Boteram cave | 16044, 797
+WhileTrue Zone | -3557, -1100`;
+/*
+  Votgil tunnel: -3548+, -1100
+  Lone Oven | -4683, -6020
+  Lone Hospital | -4794, -6035
+*/
 
 var restAreas = [];
+var pauseOnRestArea = false;
+
+function loadRestAreas (areas)
+{
+    areas = areas.split ("\n");
+    var posstrings = areas.map(area => area.substring(area.indexOf("|") + 1));
+    var poses = [];
+    restAreas.length = posstrings.length;
+    for (var i = 0; i < areas.length; ++i)
+    {
+	var j = posstrings[i].indexOf(",");
+	restAreas[i] = new Pos(
+	    Number(posstrings[i].substring(0, j)),
+	    Number(posstrings[i].substring(j+1))
+	);
+    }
+}
+
+function placeSymbolTile (tile)
+{
+    setTileBufferValue(localPlayer.pos, tile);
+    addPlaceSymbolTileCommand(tile);
+}
 
 Player.prototype.stopActionInDirection = function(direction) {
     if (this == localPlayer)
@@ -59,6 +89,53 @@ function localPlayerStopWalking(direction) {
     }
 }
 
+function getTileBufferValueRelative (pos)
+{
+    var temppos = localPlayer.pos.copy();
+    temppos.add(pos);
+    return getTileBufferValue (temppos);
+}
+
+Player.prototype.getDeltaInWalkDiagL = function (dir)
+{
+    var x = dir == 0 || dir == 3 ? -1 : 1;
+    var y = dir == 0 || dir == 1 ? -1 : 1;
+    return new Pos (x, y);
+}
+Player.prototype.getDeltaInWalkDiagR = function (dir)
+{
+    var x = dir == 3 || dir == 2 ? -1 : 1;
+    var y = dir == 3 || dir == 0 ? -1 : 1;
+    return new Pos (x, y);
+}
+
+var deltaCos = [0,1,1,1,0,-1,-1,-1];
+var deltaSin = [-1,-1,0,1,1,1,0,-1];
+Player.prototype.getDeltaInWalkDirt = function (dirt) // direction = dir + .5 diag
+{
+    var x = deltaCos[dirt];
+    var y = deltaSin[dirt];
+    return new Pos (x, y);
+}
+Player.prototype.getDeltaInOffsetDirt = function (direction, dirt)
+{
+    return this.getDeltaInWalkDirt ((direction * 2 + dirt) & 7);
+}
+
+Player.prototype.canWalkThroughRelative = function (pos)
+{
+    var ppos = this.pos.copy();
+    ppos.add(pos);
+    return this.canWalkThroughTile (getTileBufferValue (ppos));
+}
+
+Player.prototype.walkMotion = function (direction)
+{
+    var tempPos = this.getPosInWalkDirection(direction);
+    this.pos.set(tempPos);
+    placeLocalPlayerTrail(this.pos);
+    addWalkCommand(direction);
+}
 
 Player.prototype.walk = function(direction) {
     if (this.walkDelay > 0) {
@@ -73,11 +150,24 @@ Player.prototype.walk = function(direction) {
     var tempPos = this.getPosInWalkDirection(direction);
     var tempTile = getTileBufferValue(tempPos);
     if (!this.canWalkThroughTile(tempTile)) {
-        return false;
+	var tempPos2, tempPos3;
+	tempPos2 = this.getDeltaInOffsetDirt (direction, -1);
+	tempPos3 = this.getDeltaInOffsetDirt (direction, -2);
+	if (this.canWalkThroughRelative (tempPos2)
+	    && this.canWalkThroughRelative (tempPos3))
+	    this.walkMotion ((direction - 1) & 3);
+	else
+	{
+	    tempPos2 = this.getDeltaInOffsetDirt (direction, 1);
+	    tempPos3 = this.getDeltaInOffsetDirt (direction, 2);
+	    if (this.canWalkThroughRelative (tempPos2)
+		&& this.canWalkThroughRelative (tempPos3))
+		this.walkMotion ((direction + 1) & 3);
+	    else
+		return false;
+	}
     }
-    addWalkCommand(direction);
-    this.pos.set(tempPos);
-    placeLocalPlayerTrail(this.pos);
+    this.walkMotion (direction);
     this.walkDelay = (1 / 16) * framesPerSecond;
     return true;
 }
@@ -149,6 +239,35 @@ Player.prototype.teleportNearest = function (tiles)
 	return false;
 }
 
+function performAddChatMessageCommand(command) {
+    var tempPlayerName;
+    if (command.username === null) {
+        tempPlayerName = null;
+    } else {
+        tempPlayerName = encodeHtmlEntity(command.username);
+    }
+    var tempText = encodeHtmlEntity(command.text);
+    if (command.text == "report")
+	addAddChatMessageCommand (getBasicStatusString());
+    var tempIsAtBottom = (chatOutput.scrollTop + 150 > chatOutput.scrollHeight - 30);
+    var tempTag = document.createElement("div");
+    if (tempPlayerName === null) {
+        tempTag.innerHTML = tempText;
+    } else {
+        tempTag.innerHTML = "<strong>" + tempPlayerName + ":</strong> " + tempText;
+    }
+    chatOutput.appendChild(tempTag);
+    chatMessageTagList.push(tempTag);
+    while (chatMessageTagList.length > maximumChatMessageCount) {
+        var tempTag = chatMessageTagList[0];
+        chatOutput.removeChild(tempTag);
+        chatMessageTagList.splice(0, 1);
+    }
+    if (tempIsAtBottom) {
+        chatOutput.scrollTop = chatOutput.scrollHeight;
+    }
+    new OverlayChatMessage(tempPlayerName, tempText);
+}
 
 function performSetTilesCommand(command) {
     var tempPos = createPosFromJson(command.pos);
@@ -164,7 +283,10 @@ function performSetTilesCommand(command) {
         tempPos2.add(tempOffset);
         setTileBufferValue(tempPos2, tempTile);
 	if ((tempTile == ovenTile || tempTile == hospitalTile) && tempPos2.getOrthogonalDistance(new Pos(0,0)) > 4)
+	{
 	    console.log(tempPos2.x + (tempTile == ovenTile ? 2 : -2), tempPos2.y);
+	    hasStopped |= pauseOnRestArea;
+	}
         index += 1;
         tempOffset.x += 1;
         if (tempOffset.x >= tempSize) {
@@ -329,6 +451,87 @@ function keyUpEvent(event) {
     }
 }
 
+
+var clearDark = function()
+{
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvasSize, canvasSize);
+}
+
+function setDarkMode()
+{
+    clearCanvas = clearDark;
+    var spritesImageTemp = document.createElement("canvas");
+    spritesImageTemp.width = spritesImage.width;
+    spritesImageTemp.height = spritesImage.height;
+    var tempctx = spritesImageTemp.getContext("2d");
+    tempctx.drawImage(spritesImage, 0, 0);
+    tempctx.globalCompositeOperation = "source-atop";
+    tempctx.fillStyle = "#fff";
+    tempctx.fillRect(0, 40, 128, 48);
+    spritesImage = spritesImageTemp;
+}
+//setDarkMode();
+
+var services = [];
+
+var basicStatsDisplay, basicStatsCanvas;
+
+function getBasicStatusString ()
+{
+    var tempFlourCount = getInventoryItemByTile(flourTile).count;
+    var tempWaterCount = getInventoryItemByTile(waterTile).count;
+    var tempPowderCount = getInventoryItemByTile(powderTile).count;
+    var tempUnbakedBreadCount = Math.min(tempFlourCount, tempWaterCount, tempPowderCount);
+    var tempEffectiveBreadCount = localPlayer.breadCount + tempUnbakedBreadCount;
+    return `Bread: ${localPlayer.breadCount} (+ ${tempUnbakedBreadCount} = ${tempEffectiveBreadCount})\nHealth: ${localPlayerHealth}\nPos: ${localPlayer.pos.toString()}`;
+}
+
+function updateBasicStats ()
+{
+    basicStatsDisplay.innerHTML = getBasicStatusString();
+    var ctx = basicStatsCanvas.getContext ("2d");
+    var w = basicStatsCanvas.width;
+    var h = basicStatsCanvas.height;
+    ctx.fillStyle = "#000";
+    ctx.fillRect(-w/2,-h/2,w,h);
+    ctx.fillStyle = "#a0f";
+    ctx.fillRect(-2,-2,4,4);
+    ctx.fillStyle = "#05f";
+    var tempPos = new Pos(0, 0);
+    tempPos.subtract(localPlayer.pos);
+    tempPos.scale(.01);
+    tempPos.x = Math.round(tempPos.x);
+    tempPos.y = Math.round(tempPos.y);
+    ctx.fillRect(tempPos.x - 2, tempPos.y - 2, 4, 4);
+    ctx.fillStyle = "#fff";
+    for (var restArea of restAreas)
+    {
+	tempPos = restArea.copy();
+	tempPos.subtract(localPlayer.pos);
+	tempPos.scale(.01);
+	tempPos.x = Math.round(tempPos.x);
+	tempPos.y = Math.round(tempPos.y);
+	ctx.fillRect(tempPos.x - 1, tempPos.y - 1, 2, 2);
+    }
+}
+
+function makeBasicStatsModule ()
+{
+    var div = document.createElement("div");
+    div.setAttribute ("id", "basicstats");
+    basicStatsDisplay = document.createElement("pre");
+    var parentdiv = document.getElementById("content").children[1];
+    basicStatsCanvas = document.createElement("canvas");
+    basicStatsCanvas.width = 200;
+    basicStatsCanvas.height = 200;
+    basicStatsCanvas.getContext("2d").translate(100,100);
+    div.appendChild(basicStatsDisplay);
+    div.appendChild(basicStatsCanvas);
+    parentdiv.appendChild(div);
+}
+
+var airplaneTracker = localPlayer.pos.copy();
 function timerEvent() {
     if (hasStopped) {
         return;
@@ -361,8 +564,8 @@ function timerEvent() {
         return;
     }
     
-    lastActivityTime += 1;
-    if (lastActivityTime > 10 * 60 * framesPerSecond) {
+    //lastActivityTime += 1;
+    if (lastActivityTime > 20 * 60 * framesPerSecond) {
         alert("You have been kicked due to inactivity.");
         hasStopped = true;
         window.location = "menu";
@@ -370,7 +573,7 @@ function timerEvent() {
     
     invincibilityBlinkDelay += 1;
     if (invincibilityBlinkDelay >= 4) {
-         invincibilityBlinkDelay = 0;
+        invincibilityBlinkDelay = 0;
     }
     
     if (!isRequestingGameUpdate) {
@@ -464,12 +667,206 @@ function timerEvent() {
     document.getElementById("respawnPosDistance").innerHTML = tempDistance;
     
     drawCompass();
+
+    updateBasicStats ();
+
+    if (localPlayer.pos.getOrthogonalDistance(airplaneTracker) > 32)
+    {
+	console.log("Player warped from " + airplaneTracker.toString());
+	hasStopped = true;
+	for (var service of services)
+	    service.ondie();
+    }
+    airplaneTracker.set(localPlayer.pos);
+    for (var service of services)
+	if (service.running)
+	    service.timerEvent();
+}
+
+Player.prototype.collectTileConditional = function(direction) {
+    var tempPos = this.getPosInWalkDirection(direction);
+    var tempTile = getTileBufferValue(tempPos);
+    if ((tempTile >= flourTile && tempTile <= breadTile)
+        || (tempTile >= symbolStartTile && tempTile <= symbolStartTile + symbolTileAmount)) {
+	this.collectTile(direction);
+    }
+}
+
+Player.prototype.canWalkInDir = function (dir)
+{
+    return localPlayer.canWalkThroughTile(getTileBufferValue(localPlayer.getPosInWalkDirection(dir)));
+};
+
+class Service  {
+    constructor() {
+	this.running = false;
+	services.push(this);
+    }
+    ondie() {}
+    ongetchat() {}
+    timerEvent() {}
+    start() {this.running = true;}
+    stop() {this.running = false;}
 }
 
 
+var bore = new Service();
+bore.interval = -1;
+bore.left = false;
+bore.right = false;
+bore.step = function (dir)
+{
+    var c = 1;
+    for (var entity of entityList)
+    {
+	if (!entity.username && localPlayer.pos.getOrthogonalDistance(entity.pos) < 3)
+	{
+	    c = 5;
+	    break;
+	}
+    }
+    for (var i = 0; i < c; ++i)
+    {
+	localPlayer.collectTileConditional((dir + 1) & 3);
+	localPlayer.collectTileConditional((dir - 1) & 3);
+	if (bore.right)
+	    localPlayer.placeTile((dir + 1) & 3);
+	if (bore.left)
+	    localPlayer.placeTile((dir - 1) & 3);
+	if (localPlayer.canWalkInDir(dir))
+	    localPlayer.walkMotion(dir);
+	else
+	    break;
+    }
+    var delay = 70;
+    if (!localPlayer.canWalkThroughTile(getTileBufferValue(localPlayer.getPosInWalkDirection(dir))))
+    {
+	delay = 625;
+	localPlayer.removeTile(dir);
+    }
+    bore.interval = setTimeout (function () {
+	bore.step (dir);
+    }, delay);
+};
+bore.start = function (dir)
+{
+    bore.running = true;
+    bore.step (dir);
+};
+bore.stop = function ()
+{
+    bore.running = false;
+    clearTimeout (bore.interval);
+};
+bore.ondie = function ()
+{
+    bore.stop();
+};
 
+function snakeMovePlayer (minx, maxx, yoff, callback, objecet)
+{
+    var east = (localPlayer.pos.y - yoff) & 1;
+    var dir = east ? 1 : 3;
+    if ((localPlayer.pos.x == minx && !east) ||
+	(localPlayer.pos.x == maxx && east))
+	dir = 2;
+    var blocked = !localPlayer.canWalkThroughTile(getTileBufferValue(localPlayer.getPosInWalkDirection(dir)));
+    var delay = 70;
+    if (blocked)
+    {
+	localPlayer.removeTile (dir);
+	delay = 625;
+    }
+    objecet.interval = setTimeout (function () {
+	localPlayer.walkMotion (dir);
+	callback();
+    }, delay);
+}
+
+var rule = new Service();
+rule.minx = -15;
+rule.maxx = 14;
+rule.yoff = 0;
+rule.rule = 30;
+rule.chr = 35;
+rule.interval = -1;
+rule.step = function()
+{
+    var a = getTileBufferValueRelative(new Pos(-1,-1)) == 35;
+    var b = getTileBufferValueRelative(new Pos( 0,-1)) == 35;
+    var c = getTileBufferValueRelative(new Pos( 1,-1)) == 35;
+    var d = 4 * a + 2 * b + c;
+    var r = rule.rule & (1 << d);
+    if (r)
+	placeSymbolTile (35);
+    snakeMovePlayer (rule.minx, rule.maxx, rule.yoff, rule.step, rule);
+};
+rule.start = function(minx, maxx, yoff)
+{
+    this.minx = minx;
+    this.maxx = maxx;
+    this.yoff = yoff;
+    this.running = true;
+    rule.step();
+};
+rule.stop = function()
+{
+    clearInterval(rule.interval);
+    this.running = false; // no more running
+};
+rule.ondie = function ()
+{
+    rule.stop();
+};
+
+
+var safety = new Service();
+safety.timerEvent = function() {
+    if (localPlayerHealth <= 2)
+	hasStopped = true;
+};
+safety.ondie = function()
+{
+    hasStopped = true;
+}
+
+function resetEnemies ()
+{
+    hasStopped = true;
+    setTimeout(function(){hasStopped=false;},20000)
+}
+
+function getTileBuffer ()
+{
+    var arr = [];
+    var l = 0;
+    var maxw = 0;
+    for (var i = 0; i < tileBufferSize; i++)
+    {
+	arr[i] = [];
+	arr[i].length = tileBufferSize;
+	for (var j = 0; j < tileBufferSize; j++)
+	{
+	    var val = tileBuffer[l];
+	    ++l;
+	    arr[i][j] = val == 0 ? "" : val < 16 ? "0" + val.toString(16) : val.toString(16);
+	}
+	arr[i] = arr[i].join("");
+	if (arr[i].length / 2 > maxw)
+	    maxw = arr[i].length / 2;
+    }
+    return arr.join("\n");
+}
+getTileBuffer();
+
+
+loadRestAreas(areasString);
+makeBasicStatsModule();
+showOrHideModuleByName ("stats");
 clearInterval(3);
 setInterval(timerEvent, Math.floor(1000 / framesPerSecond));
 setZoom(0);
+selectInventoryItem(7);
+centerSelectedInventoryItem();
 window.onkeydown = keyDownEvent;
 window.onkeyup = keyUpEvent;
