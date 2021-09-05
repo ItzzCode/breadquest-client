9f9cfc86892cc9665da9f1900a42c4e0b93d8a8f
munvoseli <muslinmelody@gmail.com>
2021-09-05 17:07:19 -0400

    added areas script

---
+++ areas.js
@@ -0,0 +1,36 @@
+const areasString = `\
+111 31 Spawn
+-1719 -2094 Stantown
+175 -3775 Tuxtown
+-1367 -2991 Dusty Outpost
+-815 -3617 Musty Outpost
+-4279 -10639 Mel 1
+-1731 -40 Mel 2
+-2024 -410 Mel 3
+-23 5908 Mel 4 / rainbow lounge
+-59 7651 Mel 5
+47 22628 Mel 6
+-60 25833 Mel 7
+22 37081 Mel 8, 2021-05-24
+8640 50014 Mel 9, 2021-05-24
+200 25812 Mel A, 2021-05-30
+9138 100003 Mel B, 2021-05-31
+95 98666 Mel C, 2021-05-31
+15701 -596 Wikitown
+9103 -617 Small Wikitown
+16044 797 Boteram cave
+-3557 -1100 WhileTrue Zone
+14132 12336 Boteram vertical 1
+15704 21858 Boteram vertical 2
+15678 29352 Boteram vertical 3 by melody, a few days before 2021-05-24
+15669 33596 Boteram vertical 4
+15811 42440 deep bread, 2021-03-28
+15659 68653 Boteram vertical 5 by melody, 2021-05-31
+15770 85222 Boteram vertical 6 by melody, 2021-05-31
+15598 87249 Boteram vertical 7 by melody, 2021-06-13
+15593 93143 Boteram vertical 8 by melody, 2021-06-13
+14672 99938 Boteram vertical 9 by melody, 2021-06-14
+15696 61392 Boteram vertical A by melody, 2021-09-05
+7529 22 ubq323, 2020-05-16
+11886 -176 ubq323, 2021-03-05
+9127 141 poggi big sex`;
